7de2e1f99b2f86ae6fcba657b3d17b77dc3e2386
Axel Tettenborn <23020690@woodhouse.ac.uk>
2023-12-06 16:00:26 +0000

    testing t

--- Ex3.py
+++ Ex3.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 z, t, flux = np.loadtxt("./Data/KIC006922244.tbl",unpack=True,skiprows=3)
 plt.plot(t, flux, '.')
 plt.xlabel("Time")
 plt.ylabel("Luminosity")
 plt.show()
 StarRadius = 1.451 * 7 * (10**8)
+print(t)
+print(t[0])
