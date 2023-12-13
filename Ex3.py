c6c7a77afe8252c3d2890ebc9bf3b87c7b8ca30b
Axel Tettenborn <23020690@woodhouse.ac.uk>
2023-12-13 15:59:14 +0000

    playing around with interpolation

--- Ex3.py
+++ Ex3.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import make_interp_spline 
 
 z, t, flux = np.loadtxt("./Data/KIC006922244.tbl",unpack=True,skiprows=3)
-plt.plot(t, flux, '.')
-plt.xlabel("Time")
-plt.ylabel("Luminosity")
-plt.show()
-StarRadius = 1.451 * 7 * (10**8)
-print(t)
-print(t[0])
+
+xnew = np.linspace(t.min(), t.max(), 300) 
+  
+gfg = make_interp_spline(t, flux, k=3) 
+  
+y_new = gfg(xnew) 
+  
+plt.plot(xnew, y_new, '.') 
+  
+plt.show() 
