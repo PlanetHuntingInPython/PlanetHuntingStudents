5f56e58fa8689c5dc91a3de9b53b6a076981e454
szymonator <152528552+szymonator@users.noreply.github.com>
2024-01-21 18:41:33 +0000

    Create CHI_SQUARED_TEST.py

---
+++ CHI_SQUARED_TEST.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#1/N * summation of (Fi - Fm)^2 / o^2  N times
+
+c, a, b = np.loadtxt("model.txt", unpack= True)
+data_file = "Planet-Hunting-local/KIC006922244.tbl" #data file that will be inputted
+z, x, y = np.loadtxt(data_file, unpack = True, skiprows = 3)
+
+
+
+N = len(x) 
+total1 = 0
+total2 = 0
+for i in y:
+    total1 += i**2
+    total2 += i
+mean_of_squares = total1/N
+mean = total2/N
+
+variance = mean_of_squares - mean**2
+
+
+#N = number of data recordings
+
+sum = 0
+for i in range(N):
+    sum = ((y[i] - b[i])**2)/variance
+    
+
+X_squared = (1/N)*sum
